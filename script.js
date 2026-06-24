37ad50d587f2c63bb89dc5a1cfef4b26db67b86b
herculesc <herculesc635@gmail.com>
2026-06-24 14:54:13 -0300

    Primeira versão do protfólio

---
+++ script.js
@@ -0,0 +1,11 @@
+document
+.getElementById("formulario")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert(
+        "Obrigado pelo contato! Em breve retornarei."
+    );
+
+});
